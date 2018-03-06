71717f20cb2081a9de1e1e6fa73f5842c2c612f8
Tomasz Dwojak <t.dwojak@amu.edu.pl>
2018-03-06 11:44:53 +0100

    update materials

--- gonito/solve.py
+++ gonito/solve.py
@@ -1,14 +1,16 @@
-#!/bin/python3
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 
 import pandas as pd
 from sklearn.metrics import accuracy_score
 
 
 def predict(row):
+    """ Predykcja na podstawie płci pasażera """
     if row.Sex == 'female':
         return 1
-    else:
-        return 0
+    return 0
 
 
 def main():
@@ -20,26 +22,26 @@ def main():
 
 
     # Wczytanie zbioru walidacyjnego
-    dev_X = pd.read_csv('./dev-0/in.tsv', sep='\t', names=columns[1:], header=0)
+    dev_x = pd.read_csv('./dev-0/in.tsv', sep='\t', names=columns[1:], header=0)
     dev_y = pd.read_csv('./dev-0/expected.tsv', sep='\t', names=columns[:1], header=0)
 
     # Wykorzystanie funkcji predict do predykcji odpowiedzi
-    dev_X['out'] = dev_X.apply(predict, axis=1)
+    dev_x['out'] = dev_x.apply(predict, axis=1)
 
     # Zapisanie odpowiedzi do pliku
-    dev_X['out'].to_csv('./dev-0/out.tsv', sep='\t', index=False)
+    dev_x['out'].to_csv('./dev-0/out.tsv', sep='\t', index=False)
 
     # Wyświetlenie wyniki na zbiorze walidacyjnym
-    print("ACCURACY DEV: {}".format(accuracy_score(dev_y, dev_X.out)))
+    print("ACCURACY DEV: {}".format(accuracy_score(dev_y, dev_x.out)))
 
     # Wczytanie zbioru testującego
-    test_X = pd.read_csv('./dev-0/in.tsv', sep='\t', names=columns[1:], header=0)
+    test_x = pd.read_csv('./dev-0/in.tsv', sep='\t', names=columns[1:], header=0)
 
     # Predykcja
-    test_X['out'] = test_X.apply(predict, axis=1)
+    test_x['out'] = test_x.apply(predict, axis=1)
 
     # Zapis odpowiedzi do pliku
-    test_X['out'].to_csv('./test-A/out.tsv', sep='\t', index=False)
+    test_x['out'].to_csv('./test-A/out.tsv', sep='\t', index=False)
 
 
 if __name__ == '__main__':
